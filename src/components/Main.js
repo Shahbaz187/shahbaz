aa919779cd389535bf76a183159c0e412e4d7ba7
Shahbaz187 <142571007+Shahbaz187@users.noreply.github.com>
2024-10-15 22:04:42 +0500

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -47,7 +47,7 @@ z-index:1;
 const BLOG = styled(NavLink)`
 color: ${props => props.theme.text};
 position: absolute;
-top: 40%;
+top: 50%;
 right: calc(1rem + 2vw);
 transform: rotate(90deg) translate(-50%, -50%);
 text-decoration: none;
@@ -57,7 +57,7 @@ const WORK = styled(NavLink)`
 color: ${props => props.click ? props.theme.body : props.theme.text};
 
 position: absolute;
-top: 50%;
+top: 40%;
 left: calc(1rem + 2vw);
 transform: translate(-50%, -50%) rotate(-90deg) ;
 text-decoration: none;
